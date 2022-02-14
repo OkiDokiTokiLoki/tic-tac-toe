507a4876f6249989342c4dbf01d795fea4f86981
tokiloki <codebadger1@gmail.com>
2022-02-14 11:48:29 +0200

    player object created

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+'use strict'
+
+const Player = (sign) => {
+    this.sign = sign;
+
+    const getSign = () => {
+        return sign;
+    }
+
+    return { getSign };
+};
