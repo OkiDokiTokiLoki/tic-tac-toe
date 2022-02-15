9f63d3cc30f2d0e4045985163540bef64a4b4c85
tokiloki <codebadger1@gmail.com>
2022-02-15 12:44:41 +0200

    displayBoard functional

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ const Player = (sign) => {
 
     return { getSign };
 };
+
+const gameBoard = (() => {
+
+    const board = ['', '', '', '', '', '', '', '', ''];
+
+    const setIndex = (index, sign) => { // setField
+        if (index > board.length) return;
+        board[index] = sign;
+    };
+
+    const getIndex = (index) => { // getIField
+        if (index > board.length) return;
+        return board[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++){
+            board[i] = '';
+        }
+    };
+
+    return { setIndex, getIndex, reset }
+
+})();
